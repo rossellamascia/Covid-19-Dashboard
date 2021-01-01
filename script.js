7d209c8edef28be81cfb3140d6b63f1cce6626aa
Rossella Mascia <rossella.mascia@gmail.com>
2021-01-01 23:43:45 +0100

    positivi oggi per regione

--- script.js
+++ script.js
@@ -17,6 +17,28 @@ fetch("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-c
     //totale guariti
     let totalRecovered = lastUpdatedData.map(el=>el.dimessi_guariti).reduce((t,n)=>t+n)
     document.getElementById("totalRecovered").innerHTML = totalRecovered
+    //morti totali
+    let totalDeath = lastUpdatedData.map(el=>el.deceduti).reduce((t,n)=>t+n)
+    document.getElementById("totalDeath").innerHTML = totalDeath
+    //tatale positivi
+    let totalPositive = lastUpdatedData.map(el=>el.deceduti).reduce((t,n)=>t+n)
+    document.getElementById("totalPositive").innerHTML = totalPositive
+
+    //card regioni positivi oggi
+    let cardWrapper = document.getElementById("cardWrapper")
+
+    lastUpdatedData.forEach(el => {
+        let div =  document.createElement('div')
+        div.classList.add('col-12','col-md-6','my-4')
+        div.innerHTML =
+        `
+            <div class="card-custom p-3 pb-0 h-100 rounded-3">
+                <p>${el.denominazione_regione}</p>
+                <p class="text-end fw-bold fs-4 mb-0">${el.nuovi_positivi}</p>
+            </div>
+        `  
+        cardWrapper.appendChild(div)
+    });
 
     console.log(totalCases);
 })
